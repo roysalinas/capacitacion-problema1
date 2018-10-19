d835d218bab73d6257296edb1641162c36a86d81
Roy Salinas <roy.sr15@gmail.com>
2018-10-19 15:09:19 -0500

    Otro cambio del 18/10 -> paso 8

--- makefiles/deploy-ghpages.mk
+++ makefiles/deploy-ghpages.mk
@@ -10,5 +10,11 @@ define mkdir_deploy_dir
     @if [ ! -d "$(GIT_BRANCH_DIR)" ]; then mkdir $(GIT_BRANCH_DIR); fi
 endef
 
+define git_init
+    @cd $(GIT_BRANCH_DIR) && \
+     rm -rf $(GIT_BRANCH_DIR)/.git && \
+     git init
+endef
+
 deploy.ghpages:
 	@echo 'Deploy to gh-pages...'
