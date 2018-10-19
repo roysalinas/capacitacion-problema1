3303f935a5498dd58eea25d4914fa34cce355ff6
Roy Salinas <roy.sr15@gmail.com>
2018-10-19 15:02:53 -0500

    Actualización de otro cambio al 18/10 -> paso 7

--- makefiles/deploy-ghpages.mk
+++ makefiles/deploy-ghpages.mk
@@ -1,9 +1,14 @@
 .PHONY: deploy.ghpages
+
 DEPLOY_DIR = deploy
 BUILD_DIR = build
 GIT_BRANCH = gh-pages
 GIT_PERSONAL_TOKEN = --TOKEN---
 GIT_BRANCH_DIR = $(PWD)/$(DEPLOY_DIR)/$(GIT_BRANCH)
+
+define mkdir_deploy_dir
+    @if [ ! -d "$(GIT_BRANCH_DIR)" ]; then mkdir $(GIT_BRANCH_DIR); fi
+endef
 
 deploy.ghpages:
 	@echo 'Deploy to gh-pages...'
